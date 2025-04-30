9ffb9455b456dd498d6785518f4fd0d91cc08519
danny <danny.brabant@cegepmontpetit.ca>
2025-04-30 14:25:23 -0400

    change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('ceci est un test de merge\n');
+    res.end('Image docker dev pour fonctionnilté\n');
 });
 
 server.listen(3000, () => {
